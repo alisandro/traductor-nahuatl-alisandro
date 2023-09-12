56283f7af14d593ee299e56db35b202323d2a0d5
alisandro <alisandro.1@hotmail.com>
2023-09-12 09:24:58 -0600

    se modifica paquetes de unitests

--- app/src/test/java/com/alisandro/init/initializarTest.java
+++ app/src/test/java/com/alisandro/init/initializarTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.assertEquals;
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
-import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
