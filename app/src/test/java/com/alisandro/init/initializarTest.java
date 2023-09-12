6f2c217fdee85ca39b859c0bcbd504eb097a00bd
alisandro <alisandro.1@hotmail.com>
2023-09-12 09:16:40 -0600

    junitest de java

---
+++ app/src/test/java/com/alisandro/init/initializarTest.java
@@ -0,0 +1,19 @@
+package com.alisandro.init;
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class initializarTest {
+    public initializarTest()
+    {
+
+    }
+    }
